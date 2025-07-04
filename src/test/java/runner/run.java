a087222ee522176a75c0c672c6413c2652420bac
Sachin Gadekar <119165108+gadekarsachin@users.noreply.github.com>
2025-07-04 13:20:41 +0530

    Update run.java

--- src/test/java/runner/run.java
+++ src/test/java/runner/run.java
@@ -6,19 +6,13 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-
-<<<<<<< Updated upstream
+ 
 		features = "./src/test/resources/Feature/home.feature",
 		glue = {"stepdef" ,"Hooks"},
 		plugin = {"pretty" , "html:testoutput/test-report"},
 		monochrome = true
 		)
-
-
-=======
-		features = "./src/test/resources/Feature/home.feature", glue = { "stepdef", "Hooks" }, tags = " ", plugin = {
-				"pretty", "html:testoutput/test-report" }, monochrome = true)
->>>>>>> Stashed changes
+ 
 
 public class run {
 
